3c5c1ce0da11af4bcd7bb9b008e5ffe8e4c4818f
Lennart Beneke <rulezdzn@gmail.com>
2023-11-14 13:41:44 +0100

    feat(aca): add container apps sample application

---
+++ azure/containerapps/aca-demo-bestellungsverwaltung/src/services/InventoryService.ts
@@ -0,0 +1,52 @@
+import ApiError from "../ApiError";
+import { Product } from "../types";
+export default class InventoryService {
+    apiUrl: string = '';
+    
+    constructor(apiUrl: string) {
+        if (apiUrl.endsWith('/')) {
+            apiUrl = apiUrl.substring(0, apiUrl.length - 1);
+        }
+        this.apiUrl = apiUrl;
+    }
+
+    async getAllProducts(): Promise<Product[]> {
+        const res = await fetch(this.apiUrl + '/products');
+        const products = await res.json() as Product[];
+        return products;
+    }
+
+    async getProductById(productId: number): Promise<Product> {
+        const res = await fetch(this.apiUrl + '/products/' + productId);
+
+        if (!res.ok && res.status === 404) {
+            const msg = (await res.json()).error ?? 'Resource not found';
+            throw new ApiError(msg, 404);
+        } else if (!res.ok) {
+            throw new Error('Error accessing Inventory API');
+        }
+
+        const product = await res.json() as Product;
+        return product;
+    }
+
+    async updateProductStock(productId: number, stock: number): Promise<Product> {
+        const res = await fetch(this.apiUrl + '/products/' + productId, {
+            method: 'PATCH',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({ stock })
+        });
+
+        if (!res.ok && res.status === 404) {
+            const msg = (await res.json()).error ?? 'Resource not found';
+            throw new ApiError(msg, 404);
+        } else if (!res.ok) {
+            throw new Error('Error accessing Inventory API');
+        }
+
+        const product = await res.json() as Product;
+        return product;
+    }
+}
